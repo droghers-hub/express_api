5a2c371354b146d16e0dd0426118e78439a53a8c
Shashank <shashankbabu10@gmail.com>
2025-08-10 16:39:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const app = express(); 
 // const client = require('prom-client');
 
 const userRoutes = require("./routes/UserRoute");
